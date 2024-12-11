42e5cfb468320c0a97604326cb925622cdcbc91d
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2024-12-11 23:56:11 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::post('/upload', function () {
-    $files = request()->file('files');
-
-    foreach ($files as $file) {
-        $file->store('uploads');
-    }
-
-    return response()->json([
-        'message' => 'File uploaded successfully',
-        'status' => 200
-    ]);
+    return redirect('/admin/login');
 });
