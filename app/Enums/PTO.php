3db5a9d5e9a139a50394ce4839384ea9fc129470
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2025-01-03 22:54:00 +0100

    Update PTO.php

--- app/Enums/PTO.php
+++ app/Enums/PTO.php
@@ -7,4 +7,13 @@ enum PTO: string
     case Pending = 'pending';
     case Approved = 'approved';
     case Rejected = 'rejected';
+
+    public static function toArray(): array
+    {
+        return [
+            self::Pending->value => 'Pending',
+            self::Approved->value => 'Approved',
+            self::Rejected->value => 'Rejected',
+        ];
+    }
 }
