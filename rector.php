b4bd7e60504c47cb61a7a708af29fe848804feea
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2025-01-05 00:37:40 +0100

    Update rector.php

--- rector.php
+++ rector.php
@@ -1,24 +1,27 @@
 <?php
 
 use Rector\Config\RectorConfig;
+use Rector\Php81\Rector\ClassMethod\NewInInitializerRector;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
 use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
+use RectorLaravel\Rector\Class_\ModelCastsPropertyToCastsMethodRector;
 use RectorLaravel\Set\LaravelLevelSetList;
 use RectorLaravel\Set\LaravelSetList;
 
 return RectorConfig::configure()
     // register single rule
-        ->withPaths([
+    ->withPaths([
         __DIR__ . '/app',
-        __DIR__ . '/config',
         __DIR__ . '/database',
-        __DIR__ . '/routes',
         __DIR__ . '/tests',
     ])
     ->withRules([
-        TypedPropertyFromStrictConstructorRector::class
+        TypedPropertyFromStrictConstructorRector::class,
+        NewInInitializerRector::class,
     ])
     ->withSkip([
-        \Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector::class,
+        AddOverrideAttributeToOverriddenMethodsRector::class,
+        ModelCastsPropertyToCastsMethodRector::class,
     ])
     // here we can define, what prepared sets of rules will be applied
     ->withPreparedSets(
@@ -31,7 +34,6 @@ return RectorConfig::configure()
     )
     ->withSets([
         LaravelLevelSetList::UP_TO_LARAVEL_110,
-
         LaravelSetList::LARAVEL_CODE_QUALITY,
         LaravelSetList::LARAVEL_COLLECTION,
     ])
